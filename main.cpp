0a6adc281061b5f8ef32e92d9a4daee11a964340
csamuelsm <csamuelssm@gmail.com>
2025-07-09 12:20:33 -0300

    Insert e FindNext funcionando

--- main.cpp
+++ main.cpp
@@ -4,24 +4,32 @@
 
 #define INITIAL_SIZE 16 // Escolhi o valor como uma potência de 2 não muito grande, não sei bem se existe alguma regra para o tamanho inicial da tabela
 #define P 4397 // Número primo aleatório para utilizar na função de hash
+#define M 4294967296 // Universo de chaves = 2^32
+#define MSQRT 65536 // Raiz quadrada de M
 
 using namespace std;
 
+class Node;
+class HashTable;
+class vEBTree;
+
 class Node {
     public:
-        int key;
+        int32_t key;
         unsigned short deleted : 1;
+        vEBTree* pointer;
 
         Node() {
             this->key = -1;
             this->deleted = false;
         }
 
-        int setValue(int k);
+        int32_t setValue(int32_t k);
+        void initializePointer(vEBTree* tree);
         void deleteNode();
 };
 
-int Node::setValue(int k) {
+int32_t Node::setValue(int32_t k) {
     this->key = k;
     return k;
 }
@@ -29,13 +37,20 @@ int Node::setValue(int k) {
 void Node::deleteNode() {
     // Vamos usar lazy-deletion, apenas sinalizando que o nó foi deletado
     this->deleted = true;
+    return;
+}
+
+void Node::initializePointer(vEBTree* tree) {
+    //std::cout << "Inicializando ponteiro para posicao: " << tree << endl;
+    this->pointer = tree;
+    return;
 }
 
 class HashTable {
     public:
-        int size;
+        int32_t size;
         std::vector<Node> table;
-        int filled; // Vamos armazenar quantos elementos do vetor estão preenchidos, a fim de mantermos a tabela sempre com tamanho m >= 2n, onde n é a qtde de chaves
+        int32_t filled; // Vamos armazenar quantos elementos do vetor estão preenchidos, a fim de mantermos a tabela sempre com tamanho m >= 2n, onde n é a qtde de chaves
 
         float a1, a2, a3, a4, a5; // Coeficientes aleatórios do polinômio de grau 4 para a função de hash 5-independente
 
@@ -56,15 +71,15 @@ class HashTable {
             this->a4 = distrib(gen);
             this->a5 = distrib(gen);
 
-            cout << a1 << ", " << a2 << ", " << a3 << ", " << a4 << ", " << a5 << endl;
+            //std::cout << a1 << ", " << a2 << ", " << a3 << ", " << a4 << ", " << a5 << endl;
         }
 
         void tableDoubling();
         void tableHalving();
-        int insertKey(int k);
-        int findKey(int k);
-        int deleteKey(int k);
-        int hash(int k);
+        int32_t insertKey(int32_t k);
+        std::pair<int, Node*> findKey(int32_t k);
+        int32_t deleteKey(int32_t k);
+        int hash(int32_t k);
         void printTable();
 };
 
@@ -78,7 +93,7 @@ void HashTable::tableDoubling() {
     this->filled = 0;
 
     // reinserindo as chaves na nova posição    
-    for (int i = 0; i < cp.size(); i++) {
+    for (int32_t i = 0; i < cp.size(); i++) {
         if (cp[i].key != -1 && cp[i].deleted != true) {
             this->insertKey(cp[i].key);
         }
@@ -86,35 +101,38 @@ void HashTable::tableDoubling() {
 }
 
 void HashTable::tableHalving() {
-    std::vector<Node> aux((int) this->size/2);
+    std::vector<Node> aux((int32_t) this->size/2);
     std::vector<Node> cp(this->table);
 
     // "metadeando" o vetor
-    this->size = (int) this->size/2;
+    this->size = (int32_t) this->size/2;
     this->table = aux;
     this->filled = 0;
 
     // reinserindo as chaves na nova posição    
-    for (int i = 0; i < cp.size(); i++) {
+    for (int32_t i = 0; i < cp.size(); i++) {
         if (cp[i].key != -1 && cp[i].deleted != true) {
             this->insertKey(cp[i].key);
         }
     }
 }
 
-int HashTable::hash(int k) {
+int HashTable::hash(int32_t k) {
     // Vamos utilzar uma função de hash 5-independente, a fim de obtermos tempo esperado de O(1)/op
-    double sum = this->a5*std::pow(k,4) + this->a4*std::pow(k,3) + this->a3*std::pow(k,2) + this->a2*k + this->a1;
-    return ((long int) sum % P) % this->size;
+    unsigned long long int sum = (unsigned long long int) this->a5*std::pow(k,4) 
+    + (unsigned long long int) this->a4*std::pow(k,3) + 
+    (unsigned long long int) this->a3*std::pow(k,2) + 
+    (unsigned long long int) this->a2*k + this->a1;
+    return (sum % P) % this->size;
 }
 
-int HashTable::insertKey(int k) {
+int32_t HashTable::insertKey(int32_t k) {
 
     Node node = Node();
     node.setValue(k);
 
-    int idx = this->hash(k);
-    cout << "Inserindo na posição: " << idx << endl;
+    int32_t idx = this->hash(k);
+    //cout << "Inserindo na posição: " << idx << endl;
 
 
     while (this->table[idx].key != -1 && this->table[idx].deleted != true && this->table[idx].key != k) {
@@ -138,7 +156,7 @@ int HashTable::insertKey(int k) {
     return idx;
 }
 
-int HashTable::findKey(int k) {
+std::pair<int, Node*> HashTable::findKey(int32_t k) {
     int idx = this->hash(k);
     int idx_ = idx;
 
@@ -151,19 +169,19 @@ int HashTable::findKey(int k) {
 
     if (this->table[idx_].key == -1 || idx_ == idx-1) {
         // não encontramos o elemento ou demos a volta no vetor
-        return -1;
+        return std::pair<int, Node*> {-1, &this->table[idx_]};
     }
 
     if (this->table[idx_].key == k && this->table[idx_].deleted == true) {
         // elemento existe mas foi deletado
-        return -1;
+        return std::pair<int, Node*> {-1, &this->table[idx_]};
     }
 
-    return idx;
+    return std::pair<int, Node*> {idx_, &this->table[idx_]};
 }
 
-int HashTable::deleteKey(int k) {
-    int idx = this->findKey(k);
+int32_t HashTable::deleteKey(int32_t k) {
+    int32_t idx = this->findKey(k).first;
 
     if (idx == -1) {
         return -1; // Nó não existe ou já foi deletado
@@ -181,7 +199,7 @@ int HashTable::deleteKey(int k) {
 
 void HashTable::printTable() {
     std::cout<< this->filled << " POSITIONS FILLED" << endl << "|";
-    for (int i = 0; i < this->size; i++) {
+    for (int32_t i = 0; i < this->size; i++) {
         if (this->table[i].key != -1 && this->table[i].deleted != true) {
             std::cout << " " << this->table[i].key << " |";
         } else {
@@ -191,19 +209,139 @@ void HashTable::printTable() {
     std::cout << endl << endl;
 }
 
+class vEBTree {
+    public:
+        int32_t min;
+        int32_t max;
+
+        HashTable clusters;
+        vEBTree* resumo;
+
+        vEBTree() {
+            this->min = INT32_MAX;
+            this->max = INT32_MIN;
+            this->clusters = HashTable();
+        }
+
+        int32_t insert(int32_t x);
+        long int findNext(int32_t x);
+};
+
+int32_t vEBTree::insert(int32_t x) {
+    if (this->min == x || this->max == x) {
+        // Elemento já foi inserido
+        return x;
+    }
+
+    if (this->min > this->max) {
+        // Árvore está vazia
+        this->min = x;
+        this->max = x;
+        return x;
+    }
+
+    if (x < this->min) {
+        // swap(x, T.min)
+        int32_t aux = x;
+        x = this->min;
+        this->min = aux;
+    }
+
+    if (x > this->max) {
+        this->max = x;
+    }
+
+    int i = floor( ((float) x) / MSQRT );
+    int lo = x % MSQRT;
+
+    if (this->clusters.findKey(i).second->pointer == NULL) {
+        // Criando um novo cluster caso esteja vazio
+        //std::cout << "Cluster da posicao " << i << " esta vazio. Inicializando cluster... " << this->clusters.findKey(i).second->pointer << endl;
+        vEBTree* cluster = new vEBTree();
+        this->clusters.findKey(i).second->initializePointer(cluster);
+        //std::cout << "Cluster da posicao " << i << " agora esta inicializado na posicao " << cluster << " / " << this->clusters.findKey(i).second->pointer << endl << endl;
+    }
+
+    // Inserindo no cluster i
+    vEBTree* ithCluster = this->clusters.findKey(i).second->pointer;
+    ithCluster->insert(lo);
+
+    if (ithCluster->min == ithCluster->max) {
+        if (this->resumo == NULL) {
+            // Inicializando a árvore resumo caso ela não exista
+            this->resumo = new vEBTree();
+        }
+        this->resumo->insert(i);
+    }
+
+    return x;
+}
+
+long int vEBTree::findNext(int32_t x) {
+    if (x < this->min) {
+         return this->min;
+    }
+
+    if (x >= this->max) {
+        return M;
+    }
+
+    int i = floor( ((float) x) / MSQRT );
+    int lo = x % MSQRT;
+
+    vEBTree* ithCluster = this->clusters.findKey(i).second->pointer;
+
+    if (lo < ithCluster->max) {
+        return (MSQRT * i) + ithCluster->findNext(lo);
+    }
+
+    int32_t j = this->resumo->findNext(i);
+    return (MSQRT * j) + this->clusters.findKey(j).second->pointer->min;
+}
+
 int main() {
 
-    HashTable table = HashTable();
+    //HashTable table = HashTable();
+    vEBTree tree = vEBTree();
 
-    int k=0;
+    int32_t k=0;
+    unsigned int c;
 
-    while (k!=-1) {
-        std::cout << "Digite um valor para inserir na tabela: ";
-        std::cin >> k;
-        std::cout << endl;
+    while (k != -1 && c != -1) {
 
-        int idx = table.insertKey(k);
-        table.printTable();
+        std::cout << "Digite 1 para inserir e 2 para achar o sucessor: ";
+        std::cin >> c;
+
+        switch (c)
+        {
+        case 1:
+            {
+                std::cout << "Digite um valor para inserir na arvore: ";
+                std::cin >> k;
+                std::cout << endl;
+
+                tree.insert(k);
+
+                //int idx = table.insertKey(k);
+                //table.printTable();
+            }
+            break;
+        case 2:
+            {
+                std::cout << "Digite um valor para encontrar o sucessor na arvore: ";
+                std::cin >> k;
+                std::cout << endl;
+
+                int32_t res = tree.findNext(k);
+                std::cout << "Sucessor de " << k << " eh: " << res << endl << endl;
+
+                //int idx = table.deleteKey(k);
+                //table.printTable();
+            }
+            break;
+        default:
+            break;
+        }
     }
 
     return 0;
